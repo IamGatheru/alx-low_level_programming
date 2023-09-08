6c691ac47ee3b164c2e262bc8e2531599a1ea71a
IamGatheru <davidgatheru490@gmail.com>
2023-09-08 16:35:35 -0700

    Well i didn't putchar required no quotes while referring to an object, and that double quote never work on putchar with a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
