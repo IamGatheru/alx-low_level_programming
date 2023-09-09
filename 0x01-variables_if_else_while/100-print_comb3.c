08aa926664fed8f62d1204e0ee55eefc5b8e6682
IamGatheru <davidgatheru490@gmail.com>
2023-09-09 00:57:18 -0700

    Used ascii values instead of normal integers'

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,19 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 49; j <= 57; j++)
 		{
-			if (i != j)
+			if (i < j)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				if (i != 56 || j != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
