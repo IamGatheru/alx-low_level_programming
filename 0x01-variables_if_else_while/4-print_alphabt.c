7cf2290343bf7ef4d49ff43ea92a2474f063d715
IamGatheru <davidgatheru490@gmail.com>
2023-09-08 22:39:14 -0700

    nested an if loop inside a for loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase except q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
