be9eae00a126b6251e2f598e490b20a57a662fb5
IamGatheru <davidgatheru490@gmail.com>
2023-09-28 00:28:17 -0700

    Found a simpler way to iterate a loop till the terminating NULL byte is reached

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line.
+ * @s: string to be printed
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)/*If s is not the terminating Nullbyte*/
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
