048c11a42354619c60b86d97a3402001362693bd
IamGatheru <davidgatheru490@gmail.com>
2023-10-03 17:28:28 -0700

    _strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,17 +8,43 @@
  */
 
 char *_strdup(char *str)
+		
 {
-	unsigned int i, size = 0;
+		
 	char *str1;
+		
+	int i, size = 0;
+		
 
+		
 	if (str == NULL)
+		
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+		
+	i = 0;
+		
+	while (str[i] != '\0')
+		
+		i++;
+		
+
+		
 	str1 = malloc(sizeof(char) * (i + 1));
+		
+
+		
 	if (str1 == NULL)
+		
 		return (NULL);
+		
+
+		
 	for (size = 0; str[size]; size++)
+		
 		str1[size] = str[size];
+		
+
+		
 	return (str1);
+		
 }
