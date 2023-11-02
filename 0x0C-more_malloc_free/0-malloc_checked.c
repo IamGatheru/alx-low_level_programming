18fdc9ae5a7972f85710246d3956bbcab965ea5b
IamGatheru <davidgatheru490@gmail.com>
2023-11-02 13:25:10 -0700

    First use of exit instead of return

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: parameter
+ * Return: 98 if malloc fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr != NULL)
+		return (ptr);
+	else
+		exit (98);
+}
