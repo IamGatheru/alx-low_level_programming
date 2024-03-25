dca8a874654c763ab679ca18ba9605f2f35d1c99
IamGatheru <davidgatheru490@gmail.com>
2024-03-25 23:14:23 +0300

    Take 1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,9 +14,21 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	for (i =  len / 2; i < len; i++)
-	{
+    if (len % 2 == 0)
+    {
+      for (i =  len / 2; i < len; i++)
+      {
 		_putchar(str[i]);
-	}
+      }
+    }
+    else
+    {
+      for(i = (len - 1) / 2; i < len; i++)
+      {
+        _putchar(str[i]);
+      }
+    }
+
+
 	_putchar('\n');
 }
