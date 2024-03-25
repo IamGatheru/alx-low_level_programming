5409960dca136ac7e5309b247a34e865fdcf3d19
IamGatheru <davidgatheru490@gmail.com>
2024-03-26 00:58:41 +0300

    take 2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,8 @@ void puts_half(char *str)
 	int len, n;
 
 	len = 0;
-
     while (str[len] != '\0')
-    {
       len++;
-    }
     if (len % 2 == 0)
       n = len / 2;
     else if (len % 2 == 1)
