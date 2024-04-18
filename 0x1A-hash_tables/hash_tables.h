d31454af9a5872f4b49f48d166170b88cee2a345
davidgatheru490@gmail.com <davidgatheru490@gmail.com>
2024-04-19 00:30:18 +0300

    function that creates a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -31,11 +31,11 @@ typedef struct hash_table_s
 }hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-unsigned lng int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, conat char *value);
-char *hash_table_get(const hash_table *ht, const char *key);
-void hash_table_print(const_hash_table_t *ht);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 #endif
